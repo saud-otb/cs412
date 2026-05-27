55a37a43369e4cbaac5188d03715f8eababb5e3d
Saud Alotaibi <saud665@bu.edu>
2026-05-27 17:57:43 -0400

    Created the show profile page

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.conf import settings
-from .views import ProfileListView
+from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
-    path(r'', ProfileListView.as_view(), name='show_all_profiles')
+    path(r'', ProfileListView.as_view(), name='show_all_profiles'),
+    path(r'<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
+    
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Profile
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class ProfileListView(ListView):
@@ -8,3 +8,9 @@ class ProfileListView(ListView):
     model = Profile
     template_name = 'mini_insta/show_all.html'
     context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
