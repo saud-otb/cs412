d20c80b8d609e26810f5721a637c6a9b2f877396
Saud Alotaibi <saud665@bu.edu>
2026-05-31 23:07:52 -0400

    Created the form to collect input to make a new post

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf import settings
-from .views import ProfileListView, ProfileDetailView, PostDetailView
+from .views import ProfileListView, ProfileDetailView, PostDetailView, CreatePostView
 
 urlpatterns = [
     path(r'', ProfileListView.as_view(), name='show_all_profiles'),
     path(r'<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
-    path(r'post/<int:pk>/', PostDetailView.as_view(), name='show_post')
+    path(r'post/<int:pk>/', PostDetailView.as_view(), name='show_post'),
+    path(r'profile/<int:pk>/create_post', CreatePostView.as_view(), name='create_post')
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Profile, Post
-from django.views.generic import ListView, DetailView
+from .models import Profile, Post, Photo
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import CreatePostForm
 
 # Create your views here.
 class ProfileListView(ListView):
@@ -20,3 +21,28 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'mini_insta/show_post.html'
     context_object_name = 'post'
+
+class CreatePostView(CreateView):
+    form_class = CreatePostForm
+    template_name = 'mini_insta/create_post_form.html'
+
+    def get_context_data(self, **kwargs):
+        context_dictionary = super().get_context_data(**kwargs)
+
+        pk = self.kwargs['pk']
+        profile = Profile.objects.get(pk=pk)
+        context_dictionary['profile'] = profile
+
+        return context_dictionary
+
+    def form_valid(self, form):
+        pk = self.kwargs['pk']
+        profile = Profile.objects.get(pk=pk)
+        form.instance.profile = profile
+
+        image_url = self.request.POST['image_url']
+
+        Photo.objects.create(post=self.object, image_url=image_url)
+        
+        return super.form_valid(form)
+
